53041876e25ab3f81ef568070b42f1d3fe7c2e56
Ivan Ng <ivan.ng0515@gmail.com>
2023-06-03 19:34:40 +0800

    commit

--- cal/script.js
+++ cal/script.js
@@ -1,14 +1,8 @@
 const specific_date = new Date('2023-6-1');
 
-const arr1 = [0, 0, 0, 0, 0, 0, 0];
-const arr2 = [0, 0, 0, 0, 0, 0, 0];
-const arr3 = [0, 0, 0, 0, 0, 0, 0];
-const arr4 = [0, 0, 0, 0, 0, 0, 0];
-const arr5 = [0, 0, 0, 0, 0, 0, 0];
+const month = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-const append = (week1, week2, week3, week4, week5, date) => {
-    if (date.getDay === 0) {
+const append = () => {
     
-    }
 }
 
