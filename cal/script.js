1b68f8d7a3a63218a1946f02af7b2e377b6ce910
Ivan Ng <ivan.ng0515@gmail.com>
2023-06-21 23:08:07 +0800

    finally

--- cal/script.js
+++ cal/script.js
@@ -1,48 +1,79 @@
-const specific_date = new Date('2023-06-01');
+const specific_date = new Date('2023-04-01');
 
 const month = [];
 
-const getDayCount = (year, month) => {
+const getDays = (year, month) => {
+    let days = 0;
     if (month === 2) {
         if (year % 4 === 0) {
             if (year % 100 === 0) {
                 if (year % 400 === 0) {
-                    return 29; // Leap year
+                    days = 29;
+                    return days; // Leap year
                 } else {
-                    return 28; // Not a leap year
+                    days = 28;
+                    return days; // Not a leap year
                 }
             } else {
-            return 29; // Leap year
+                days = 29;
+                return days; // Leap year
             }
         } else {
-            return 28; // Not a leap year
+            days = 28;
+            return days; // Not a leap year
         }
-    } else if (month === 1, 3, 5, 7, 8, 10, 12) {
-        return 31;
+    } else if (month === 1, month === 3, month === 5, month === 7, month === 8, month === 10, month === 12) {
+        days = 31;
+        return days;
     } else {
-        return 30
+        days = 30;
+        return days;
     }
         
 }
 
-const push_zero = (cnt) => {
+const push_zero = (total) => {
     let push_times = 0;
-    while (push_times != cnt) {
-        month.append(0);
+    while (push_times != total) {
+        month.push(0);
         push_times += 1;
     }
 } 
 
-const append = (day_cnt) => {
-    let cnt = 1;
-    if (date.getDay === 0) {
-        while (cnt != day_cnt) {
-            month.append(cnt);
-            cnt += 1;
-        }
-    } else if (date.getDay === 1) {
-        push_zero(1);
-
+const pushUntilFull = (count, totalDays) => {
+    while (count != totalDays) {
+        count += 1;
+        month.push(count);
     }
 }
 
+const append = (day_cnt) => {
+    let cnt = 0;
+    if (specific_date.getDay() === 0) {
+        pushUntilFull(cnt, day_cnt);
+    } else if (specific_date.getDay() === 1) {
+        push_zero(1);
+        pushUntilFull(cnt, day_cnt);
+    } else if (specific_date.getDay() === 2) {
+        push_zero(2);
+        pushUntilFull(cnt, day_cnt);
+    } else if (specific_date.getDay() === 3) {
+        push_zero(3);
+        pushUntilFull(cnt, day_cnt);
+    } else if (specific_date.getDay() === 4) {
+        push_zero(4);
+        pushUntilFull(cnt, day_cnt);
+    } else if (specific_date.getDay() === 5) {
+        push_zero(5);
+        pushUntilFull(cnt, day_cnt);
+    } else if (specific_date.getDay() === 6) {
+        push_zero(6);
+        pushUntilFull(cnt, day_cnt);
+    }
+}
+
+var noOfDays = getDays(2023, 4);
+
+append(noOfDays);
+
+console.log(month);
---
+++ cal/test.js
@@ -0,0 +1,8 @@
+let cnt = 0;
+
+while (cnt != 10) {
+    cnt += 1;
+    console.log(cnt);
+}
+
+console.log("i'm here")
