00f61ba3d4ce0d52d86db4a82afba6908068c52a
Ivan NG <ivan.ng0515@gmail.com>
2023-07-10 16:55:11 +0800

    closes #1 goToFloor function typeof requested_floor is NaN

--- elevator/script.js
+++ elevator/script.js
@@ -5,9 +5,6 @@ const elevator1_cur_floor = document.getElementById("elevator1_cur_floor");
 const elevator2_cur_floor = document.getElementById("elevator2_cur_floor");
 const elevator3_cur_floor = document.getElementById("elevator3_cur_floor");
 const request_floor = document.getElementById("select_floor");
-const request_floor_val = request_floor.value;
-console.log(request_floor.value);
-
 
 const max_floor = 10;
 const min_floor = 1;
@@ -15,26 +12,34 @@ const min_floor = 1;
 const getRandomFloor  = (min, max) => {
     min = Math.floor(min);
     max = Math.ceil(max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
+    return Math.floor(Math.random() * (max - min + 1) + min).toString();
 }
 
+const compare = (elevator1, elevator2, elevator3, requested_floor, cur_floor) => {
+    if (cur_floor > requested_floor) {
+        const goUp = false;
+    } else {
+        const goUp = true;
+    }
+
+    if (goUp) {
+        
+    }
+};
 
 const goToFloor = (requested_floor, current_floor) => {
-    console.log(typeof requested_floor, requested_floor);
-    console.log(typeof current_floor, current_floor);
-    if (requested_floor === current_floor || isNaN(requested_floor) || request_floor > 10 || request_floor < 1) {
-        console.log("here")
+    if (requested_floor === current_floor || isNaN(requested_floor) || requested_floor > 10 || requested_floor < 1) {
         return;
     } else {
         console.log("hello world");
     }
 }
 
-usr_floor.innerHTML = "Current Floor: " + getRandomFloor(min_floor, max_floor).toString();
-elevator1_cur_floor.innerHTML = "Elevator 1 Current Floor: " + getRandomFloor(min_floor, max_floor).toString();
-elevator2_cur_floor.innerHTML = "Elevator 2 Current Floor: " + getRandomFloor(min_floor, max_floor).toString();
-elevator3_cur_floor.innerHTML = "Elevator 3 Current Floor: " + getRandomFloor(min_floor, max_floor).toString();
+usr_floor.innerHTML = "Current Floor: " + getRandomFloor(min_floor, max_floor);
+elevator1_cur_floor.innerHTML = "Elevator 1 Current Floor: " + getRandomFloor(min_floor, max_floor);
+elevator2_cur_floor.innerHTML = "Elevator 2 Current Floor: " + getRandomFloor(min_floor, max_floor);
+elevator3_cur_floor.innerHTML = "Elevator 3 Current Floor: " + getRandomFloor(min_floor, max_floor);
 
 btn.addEventListener("click", function() {
-    goToFloor(parseInt(request_floor_val), parseInt(usr_floor.innerHTML.replace(/Current Floor: /g, '')));
+    goToFloor(parseInt(request_floor.value), parseInt(usr_floor.innerHTML.replace(/Current Floor: /g, '')));
 });
