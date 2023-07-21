9fbe1115471963cc3ffd244a609677434e680ed6
Ivan NG <ivan.ng0515@gmail.com>
2023-07-21 14:35:22 +0800

    commit

--- elevator/script.js
+++ elevator/script.js
@@ -6,6 +6,10 @@ const elevator2_cur_floor = document.getElementById("elevator2_cur_floor");
 const elevator3_cur_floor = document.getElementById("elevator3_cur_floor");
 const request_floor = document.getElementById("select_floor");
 
+const e1text = "Elevator 1 Current Floor: ";
+const e2text = "Elevator 2 Current Floor: ";
+const e3text = "Elevator 3 Current Floor: ";
+
 const max_floor = 10;
 const min_floor = 1;
 
@@ -16,29 +20,52 @@ const getRandomFloor  = (min, max) => {
 }
 
 const compare = (elevator1, elevator2, elevator3, requested_floor, cur_floor) => {
-    if (cur_floor > requested_floor) {
-        const goUp = false;
-    } else {
-        const goUp = true;
+    var elevators_to_usr = [elevator1 - cur_floor, elevator2 - cur_floor, elevator3 - cur_floor];
+
+    let usr_to_destination = requested_floor - cur_floor;
+
+    for (let i = 0; i < 3; i++) {
+        if (elevators_to_usr[i] < 0) {
+            elevators_to_usr[i] *= -1;
+            elevators_to_usr += usr_to_destination;
+        }
     }
 
-    if (goUp) {
-        
-    }
+    let cloest_to_usr = Math.min(...elevators_to_usr);
+
+    return cloest_to_usr;
+
 };
 
 const goToFloor = (requested_floor, current_floor) => {
     if (requested_floor === current_floor || isNaN(requested_floor) || requested_floor > 10 || requested_floor < 1) {
         return;
     } else {
-        console.log("hello world");
+        let cloest = compare(elevator1_cur_floor, elevator2_cur_floor, elevator3_cur_floor, requested_floor, current_floor);
+
+        for (let i = 0; i < 3; i++) {
+            if (elevators_to_usr[i] == closet) {
+                let elevator_code = i;
+            }
+        }
+
+        if (elevator_code === 0) {
+            usr_floor.innerHTML = "Current Floor: " + requested_floor.toString();
+            elevator1_cur_floor.innerHTML = e1text + + requested_floor.toString();
+        } else if (elevator_code === 1) {
+            usr_floor.innerHTML = "Current Floor: " + requested_floor.toString();
+            elevator2_cur_floor.innerHTML = e2text + + requested_floor.toString();
+        } else if (elevator_code === 2) {
+            usr_floor.innerHTML = "Current Floor: " + requested_floor.toString();
+            elevator3_cur_floor.innerHTML = e3text + + requested_floor.toString();
+        }
     }
 }
 
 usr_floor.innerHTML = "Current Floor: " + getRandomFloor(min_floor, max_floor);
-elevator1_cur_floor.innerHTML = "Elevator 1 Current Floor: " + getRandomFloor(min_floor, max_floor);
-elevator2_cur_floor.innerHTML = "Elevator 2 Current Floor: " + getRandomFloor(min_floor, max_floor);
-elevator3_cur_floor.innerHTML = "Elevator 3 Current Floor: " + getRandomFloor(min_floor, max_floor);
+elevator1_cur_floor.innerHTML = e1text + getRandomFloor(min_floor, max_floor);
+elevator2_cur_floor.innerHTML = e2text + getRandomFloor(min_floor, max_floor);
+elevator3_cur_floor.innerHTML = e3text + getRandomFloor(min_floor, max_floor);
 
 btn.addEventListener("click", function() {
     goToFloor(parseInt(request_floor.value), parseInt(usr_floor.innerHTML.replace(/Current Floor: /g, '')));
