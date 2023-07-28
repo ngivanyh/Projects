7cecb0efb01c4f8c5904045da29062af1f3a657c
Ivan NG <ivan.ng0515@gmail.com>
2023-07-28 15:37:32 +0800

    more things on bubble sort

--- bubble_sort/bubble.c
+++ bubble_sort/bubble.c
@@ -3,15 +3,23 @@
 #include <stdbool.h>
 
 bool is_num(char **input, int length);
+int sort(int numbers);
 
 int main(int argc, char **argv)
 {
-    if (argc < 1 || is_num(argv, argc) == 0)
+    if (argc < 2 || is_num(argv, argc) == 0)
     {
         printf("Usage: ./bubble num1 num2 num3\n");
+        return 1;
     }
     else
     {
+        int sort_array[argc - 1];
+
+        for (int i = 1; i < argc - 1; i++)
+        {
+            sort_array[i - 1] = atoi(argv[i]);
+        }
         printf("Hello world\n");
     }
 }
@@ -22,7 +30,7 @@ bool is_num(char **input, int length)
 
     for (int i = 1; i < length - 1; i++)
     {
-        if (atoi(input[1]) == 0)
+        if (atoi(input[i]) == 0)
         {
             break;
             is_num = false;
