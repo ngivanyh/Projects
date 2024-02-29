a25356d0e46993820518d6a7effc0d5dacfd91db
Ivan NG <ivan.ng0515@gmail.com>
2024-02-29 22:17:05 +0800

    BOMBSHELL COMMIT

---
+++ typing_working/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <p className="">Adipisicing exercitation cupidatat velit adipisicing sit incididunt mollit.</p>
+      <textarea class="txtarea"></textarea>
+    </div>
+  );
+}
+
+export default App;
