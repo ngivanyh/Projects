4202738ac08189b33a8e6e01b954bf543e849bce
ngivanyh <ivan.ng0515@gmail.com>
2025-11-04 22:07:39 +0800

    clean and concisely finished

--- apcs_tree/tree2.py
+++ apcs_tree/tree2.py
@@ -1,5 +1,5 @@
 class node:
-    def __init__(self, value, parent=None):
+    def __init__(self, value: int, parent: node | None=None):
         self.value = value
         self.parent = parent
         
@@ -25,21 +25,33 @@ class node:
             return False
         return True
     
-    def addChild(self, child):
+    def addChild(self, child: node):
         if not self.hasSpace():
             return None
         
         for i, val in enumerate(self.children.values()):
-            if val == None: self.children[i + 1] = child; break
-            
+            if val == None: 
+                self.children[i + 1] = child
+                return (self.value, child.value)
+
 node_list = [int(n) for n in input().split(" ")]
 
 tree = node(node_list[0], None)
-cur = tree
+cur = tree; connected_pairs = []; node_value_diff = 0 # without (n, 0) pairs
 
 for n_val in node_list[1:]:
-    if cur.hasSpace():
-        new = node(n_val, cur)
-        cur.addChild(new)
-        cur = new
-    print(tree)
+    if not cur.hasSpace():
+        while (cur := cur.parent) and (not cur.hasSpace()):
+            pass
+        if cur is None:
+            break
+        
+    new = node(n_val, cur)
+    connected_pairs.append(cur.addChild(new))
+    cur = new
+
+for pair in connected_pairs:
+    if 0 not in pair:
+        node_value_diff += abs(pair[0] - pair[1])
+
+print(node_value_diff)
