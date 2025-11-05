2cd610a21f4bd0db5933a5d97e932be5c3c23403
ngivanyh <ivan.ng0515@gmail.com>
2025-11-05 19:13:52 +0800

    nice and pythonic 50 lines

--- apcs_tree/tree2.py
+++ apcs_tree/tree2.py
@@ -1,5 +1,5 @@
-class node:
-    def __init__(self, value: int, parent: node | None=None):
+class Node:
+    def __init__(self, value: int, parent: Node | None=None):
         self.value = value
         self.parent = parent
         
@@ -21,13 +21,10 @@ class node:
         return f"node(value={self.value}, childrens={self.children})"
     
     def hasSpace(self):
-        if (self.children == None) or (None not in self.children.values()):
-            return False
-        return True
+        return False if (self.children == None) or (None not in self.children.values()) else True
     
-    def addChild(self, child: node):
-        if not self.hasSpace():
-            return None
+    def addChild(self, child: Node):
+        if not self.hasSpace(): return None
         
         for i, val in enumerate(self.children.values()):
             if val == None: 
@@ -36,8 +33,8 @@ class node:
 
 node_list = [int(n) for n in input().split(" ")]
 
-tree = node(node_list[0], None)
-cur = tree; connected_pairs = []; node_value_diff = 0 # without (n, 0) pairs
+tree = Node(node_list[0], None)
+cur = tree; connected_pairs = [] # without (n, 0) pairs
 
 for n_val in node_list[1:]:
     if not cur.hasSpace():
@@ -46,12 +43,8 @@ for n_val in node_list[1:]:
         if cur is None:
             break
         
-    new = node(n_val, cur)
+    new = Node(n_val, cur)
     connected_pairs.append(cur.addChild(new))
     cur = new
 
-for pair in connected_pairs:
-    if 0 not in pair:
-        node_value_diff += abs(pair[0] - pair[1])
-
-print(node_value_diff)
+print(sum([abs(pair[0] - pair[1]) for pair in connected_pairs if 0 not in pair]))
