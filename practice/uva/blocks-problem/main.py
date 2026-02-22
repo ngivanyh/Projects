d95b78edd6056da3e8082c3aee636ff6da0a61c4
ngivanyh <ivan.ng0515@gmail.com>
2026-02-22 20:24:20 +0800

    fin: uva blocks problem

--- practice/uva/blocks-problem/main.py
+++ practice/uva/blocks-problem/main.py
@@ -1,6 +1,10 @@
 # UVa: The Blocks Problem
 
-def find_block(blocks, block):
+blocks = []
+
+def find_block(block):
+    global blocks
+
     for i, stack in enumerate(blocks):
         try:
             stack_idx = stack.index(block)
@@ -9,56 +13,71 @@ def find_block(blocks, block):
             continue
     return None
 
+def return_blocks(block):
+    global blocks
 
-def move(blocks, preposition, target, destination):
-    target_idx = find_block(blocks, target)
+    block_idx = find_block(block)
 
-    if target_idx is None:
-        print(f"Error: Cannot find target block of number {target}")
+    if block_idx is None:
         return
 
-    _ = [blocks[block].append(block) for block in blocks[target_idx[0]][target_idx[1] + 1:]]
-    blocks[target_idx[0]] = blocks[target_idx[0]][target_idx[1]:]
+    stack_length = len(blocks[block_idx[0]]) # initial stack length
+    for i in range(stack_length - 1, block_idx[1], -1):
+        move_block = blocks[block_idx[0]].pop(i) # pop returns the value of the list element removed
+        blocks[move_block].append(move_block)
+
+def move(preposition, target, destination):
+    global blocks
+
+    return_blocks(target)
 
     if preposition == "onto":
-        pass # move everything above b to their original positions
+        return_blocks(destination)
 
+    target_idx, destination_idx = find_block(target), find_block(destination)
 
-def pile(blocks, preposition, target, destination):
-    pass
+    blocks[target_idx[0]].remove(target)
+    blocks[destination_idx[0]].append(target)
 
+def pile(preposition, target, destination):
+    global blocks
+
+    if preposition == "onto":
+        return_blocks(destination)
+
+    target_idx, destination_idx = find_block(target), find_block(destination)
+
+    move_stack = blocks[target_idx[0]][target_idx[1]:].copy() # .copy() present because the need of a copy of the list, not a pointer to it
+    blocks[target_idx[0]] = blocks[target_idx[0]][:target_idx[1]]
+    blocks[destination_idx[0]] += move_stack
 
 def main():
+    global blocks
+
     blocks = [[n] for n in range(int(input()))]
 
     while (command := input()) and command != "quit":
-        try:
-            command = command.split(" ")
-        except Exception:
-            print("Error: Invalid Command")
-            break
+        command = command.split(" ")
 
-        action = command[0]
-        preposition = command[2]
+        action, preposition = command[0], command[2]
         target, destination = int(command[1]), int(command[3])
 
-        if target == destination:
-            continue
+        # invalid commands
+        if (
+            target == destination
+            or find_block(target)[0] == find_block(destination)[0]
+        ): continue
 
-        print(target, destination)
-
-        match action:
-            case "move":
-                move(blocks, preposition, target, destination)
-            case "pile":
-                pile(blocks, preposition, target, destination)
-            case _:
-                print("Error: Invalid action")
-                break
+        # action delegation
+        if action == "move":
+            move(preposition, target, destination)
+        elif action == "pile":
+            pile(preposition, target, destination)
+        else:
+            break
 
     for i, stack in enumerate(blocks):
-        print(f"{i}: {" ".join(map(str, stack))}")
-
+        print(f"{i}: {' '.join(map(str, stack))}")
 
 if __name__ == "__main__":
     main()
