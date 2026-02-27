af2aa50f658b7835d6b184699e2e33d03b55bf32
ngivanyh <ivan.ng0515@gmail.com>
2026-02-27 12:00:46 +0800

    dp version complete

--- practice/uva/ugly-numbers/better-gen.c
+++ practice/uva/ugly-numbers/better-gen.c
@@ -4,16 +4,39 @@ typedef unsigned long long ullong;
 
 #define GENERATION_TARGET 1500
 
+ullong min(ullong ns[], size_t length);
+
 int main(void)
 {
     ullong numbers[GENERATION_TARGET];
     numbers[0] = 1;
 
-    ullong candidates[3];
-    for (int i = 1; i < GENERATION_TARGET;)
+    ullong i2 = 0, i3 = 0, i5 = 0;
+    for (int i = 1; i < GENERATION_TARGET; ++i)
     {
+        ullong candidates[3] = {
+            numbers[i2] * 2,
+            numbers[i3] * 3,
+            numbers[i5] * 5
+        };
 
+        numbers[i] = min(candidates, 3);
+
+        if (numbers[i] == candidates[0]) ++i2;
+        if (numbers[i] == candidates[1]) ++i3;
+        if (numbers[i] == candidates[2]) ++i5;
     }
 
-    printf("The 1500'th ugly number is %llu.", numbers[1]);
+    printf("The 1500'th ugly number is %llu.\n", numbers[1499]);
+}
+
+ullong min(ullong ns[], size_t length)
+{
+    ullong min = ns[0];
+
+    for (size_t i = 1; i < length; ++i)
+        if (ns[i] < min)
+            min = ns[i];
+
+    return min;
 }
